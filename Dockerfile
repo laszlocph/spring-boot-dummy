929d141ef48fe39c90829b035105436b51b33c18
Laszlo Fogas <laszlo@falconsocial.com>
2017-06-14 11:53:06 +0200

    Removing the jenkins pipeline

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM anapsix/alpine-java:8_jdk
-
-WORKDIR spring-boot-dummy
-ADD . .
-CMD sleep 1h
--- build-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew clean build
-cp build/libs/spring-boot-dummy-0.1.0.jar docker/
-
-cd docker
-docker build -t laszlocph/spring-boot-dummy .
